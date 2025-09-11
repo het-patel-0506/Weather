409cbcf93dec37e6c9d3fa86e1e34e8428b4e237
I_am_Het_Patel <patelhet050603@gmail.com>
2025-09-11 11:03:25 +0530

    Add global title and favicon (sun cloud); replace alerts with toasts; coordinate-based OWM lookup and UI fixes

--- weather-dashboard/pages/_app.jsx
+++ weather-dashboard/pages/_app.jsx
@@ -1,6 +1,16 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Weather Pro</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icons/sun-cloud.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
